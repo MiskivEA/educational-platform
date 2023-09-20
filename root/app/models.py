b5f377527805788d85e99a4d16dd008808a820f5
Eugene <evgenymiskiv@gmail.com>
2023-09-20 19:54:18 +0300

    fix not/viewed

--- root/app/models.py
+++ root/app/models.py
@@ -40,27 +40,26 @@ class ProductLesson(models.Model):
 class LessonUser(models.Model):
     """Модель для связки множества уроков со множеством пользователей"""
 
-    class Status(models.IntegerChoices):
-        not_viewed = 0, 'Не просмотрено'
-        viewed = 1, 'Просмотрено'
+    class Status(models.TextChoices):
+        not_viewed = 'not_viewed', 'Не просмотрено'
+        viewed = 'viewed', 'Просмотрено'
 
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Урок')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     current_time = models.PositiveIntegerField(default=0)
-    view_status = models.BooleanField(choices=Status.choices,
-                                      default=Status.not_viewed)
+    view_status = models.TextField(choices=Status.choices, default=Status.not_viewed)
 
     def __str__(self):
         return f'{self.lesson.title} : {self.user.username}'
 
     def save(self, *args, **kwargs):
-        if self.viewed_or_not():
-            self.view_status = 1
+        if self.is_viewed():
+            self.view_status = self.Status.viewed
         else:
-            self.view_status = 0
+            self.view_status = self.Status.not_viewed
         print(self.view_status)
         super().save(*args, **kwargs)
 
-    def viewed_or_not(self, to_percent=100, limit=80):
+    def is_viewed(self, to_percent=100, limit=80):
         """Определение Просмотрено/Не просмотрено исходя из порога в 80%"""
         return (self.current_time / self.lesson.duration) * to_percent >= limit

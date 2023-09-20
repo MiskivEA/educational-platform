545cae9772184c351d39a0c10c51227411e122f5
Eugene <evgenymiskiv@gmail.com>
2023-09-20 14:12:38 +0300

    Инициализация проекта, приложения, переменных окружения

---
+++ root/root/env.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class DjangoSecretKey:
+    secret_key: str
+
+
+@dataclass
+class EnvConfigs:
+    secret_key: DjangoSecretKey
+
+
+def load_config(env_file_path=None):
+    env = Env()
+    env.read_env(env_file_path)
+
+    config = EnvConfigs(
+        secret_key=env('SECRET_KEY')
+    )
+
+    return config
